bc9ca39a5423d0bbb8864be6c35ac74520e6a329
valrichter <valenrichter11@gmail.com>
2024-08-19 23:15:13 -0300

    rename

--- src/main/java/com/book_crud/MVC_API_RESTful/repository/BookRepository.java
+++ src/main/java/com/book_crud/MVC_API_RESTful/repository/BookRepository.java
@@ -3,6 +3,6 @@ package com.book_crud.MVC_API_RESTful.repository;
 import com.book_crud.MVC_API_RESTful.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepo extends CrudRepository<Book, Integer> {
+public interface BookRepository extends CrudRepository<Book, Integer> {
 
 }

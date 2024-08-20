44411fd45fa0996e73763d3edd75aa06380c2854
valrichter <valenrichter11@gmail.com>
2024-08-19 21:30:16 -0300

    Initial commit

---
+++ src/main/java/com/book_crud/MVC_API_RESTful/MvcApiResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.book_crud.MVC_API_RESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcApiResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcApiResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book_crud/MVC_API_RESTful/MvcApiResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book_crud.MVC_API_RESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApiResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
